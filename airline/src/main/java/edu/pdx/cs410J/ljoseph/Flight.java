5f644a5d10b17ca7eb0e730842cf3e63fa06b665
London Joseph <london.joseph@pcc.edu>
2022-01-12 11:53:02 -0800

    add java koans

--- airline/src/main/java/edu/pdx/cs410J/ljoseph/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/ljoseph/Flight.java
@@ -3,6 +3,54 @@ package edu.pdx.cs410J.ljoseph;
 import edu.pdx.cs410J.AbstractFlight;
 
 public class Flight extends AbstractFlight {
+
+  private String flightNumber;
+  private String src;
+  private String depart;
+  private String dtime;
+  private String dest;
+  private String arrive;
+  private String atime;
+
+  public Flight(String flightNumber, String src, String depart, String dtime, String dest,
+                String arrive, String atime) {
+    this.flightNumber = flightNumber;
+    this.src = src;
+    this.depart = depart;
+    this.dtime = dtime;
+    this.dest = dest;
+    this.arrive = arrive;
+    this.atime = atime;
+  }
+
+  public void setFlightNumber(String flightNumber) {
+    this.flightNumber = flightNumber;
+  }
+
+  public void setSrc(String src) {
+    this.src = src;
+  }
+
+  public void setDepart(String depart) {
+    this.depart = depart;
+  }
+
+  public void setDtime(String dtime) {
+    this.dtime = dtime;
+  }
+
+  public void setDest(String dest) {
+    this.dest = dest;
+  }
+
+  public void setArrive(String arrive) {
+    this.arrive = arrive;
+  }
+
+  public void setAtime(String atime) {
+    this.atime = atime;
+  }
+
   @Override
   public int getNumber() {
     return 42;
--- airline/src/main/java/edu/pdx/cs410J/ljoseph/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/ljoseph/Project1.java
@@ -6,11 +6,22 @@ package edu.pdx.cs410J.ljoseph;
 public class Project1 {
 
   public static void main(String[] args) {
-    Flight flight = new Flight();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    String flightNumber = null;
+    String src = null;
+    String depart = null;
+    String dtime = null;
+    String dest = null;
+    String arrive = null;
+    String atime = null;
+    boolean print = false;
+    boolean readme = false;
+
+
     System.err.println("Missing command line arguments");
     for (String arg : args) {
-      System.out.println(arg);
+      ;
     }
+    Flight flight = new Flight(flightNumber, src, depart, dtime, dest, arrive, atime);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
     System.exit(1);
   }
 
