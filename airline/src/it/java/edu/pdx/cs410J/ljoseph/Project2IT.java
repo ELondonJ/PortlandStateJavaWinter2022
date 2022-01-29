2959af6139afb30e9dbb8c0bf36f2cb24b0e7de4
London Joseph <london.joseph@pcc.edu>
2022-01-29 12:21:05 -0800

    All tests passing after flight dates changes to Date objects

--- airline/src/it/java/edu/pdx/cs410J/ljoseph/Project2IT.java
+++ airline/src/it/java/edu/pdx/cs410J/ljoseph/Project2IT.java
@@ -36,21 +36,26 @@ class Project2IT extends InvokeMainTestCase {
   }
     @Test
     void testCorrectCommandLineArguments() {
-        MainMethodResult result = invokeMain("-print","Portland International","1234", "pdx", "12/12/2002","12:13","slc","12/12/2002", "14:20");
+        MainMethodResult result = invokeMain("-print","Portland International","1234", "pdx",
+                "12/12/2002","12:13","am", "slc","12/12/2002", "11:20","am");
         assertThat(result.getExitCode(), equalTo(0));
-        assertThat(result.getTextWrittenToStandardOut(), containsString("Flight 1234 departs pdx at 12/12/2002 12:13 arrives slc at 12/12/2002 14:20"));
+        assertThat(result.getTextWrittenToStandardOut(), containsString("Flight 1234 departs pdx at" +
+                " 12/12/2002 12:13 AM arrives slc at 12/12/2002 11:20"));
     }
 
 
     @Test
     void testIncorrectCommandLineArgumentsThrowsError() {
-        MainMethodResult result = invokeMain("Portland International","1234", "pdxx", "12/12/2002","12:13","slc","12/12/2002", "14:20");
+        MainMethodResult result = invokeMain("Portland International","1234", "pdxx", "12/12/2002","12:13","am",
+                "slc","12/12/2002", "14:20","am");
         assertThat(result.getExitCode(), equalTo(1));
-        assertThat(result.getTextWrittenToStandardError(), containsString("Source and Destination represented by three-letter code. ie pdx"));
+        assertThat(result.getTextWrittenToStandardError(), containsString("Source and Destination represented" +
+                " by three-letter code. ie pdx"));
     }
     @Test
     void testIncorrectFlightNumberFormatArgumentsThrowsError() {
-        MainMethodResult result = invokeMain("Portland International","aba", "pdx", "12/12/2002","12:13","slc","12/12/2002", "14:20");
+        MainMethodResult result = invokeMain("Portland International","aba", "pdx", "12/12/2002","12:13","am",
+                "slc","12/12/2002", "14:20","am");
         assertThat(result.getExitCode(), equalTo(1));
         assertThat(result.getTextWrittenToStandardError(), containsString("Incorrect flight number format"));
     }
@@ -68,7 +73,8 @@ class Project2IT extends InvokeMainTestCase {
     }
     @Test
     void toManyCommandLineArgsCommandLineArgument() {
-        MainMethodResult result = invokeMain("Portland International","345", "pdx", "12/12/2002","12:13","slc","12/12/2002", "14:20", "extra");
+        MainMethodResult result = invokeMain("Portland International","345", "pdx", "12/12/2002","12:13","am","slc",
+                "12/12/2002", "14:20","am", "extra");
         assertThat(result.getExitCode(), equalTo(1));
         assertThat(result.getTextWrittenToStandardError(), containsString("Too many command"));
     }
@@ -77,9 +83,10 @@ class Project2IT extends InvokeMainTestCase {
         File testFile = new File("airtest3.txt");
         PrintWriter pw = new PrintWriter(testFile);
         pw.println("portland int");
-        pw.println("2344 pdx 12/12/1212 12:12 slc 12/12/1212 12:33");
+        pw.println("2344 pdx 12/12/2022 12:12 am slc 12/12/2022 12:33 am");
         pw.close();
-        MainMethodResult result = invokeMain("-textFile", "airtest3.txt","Salt Lake","345", "pdx", "12/12/2002","12:13","slc","12/12/2002", "14:20");
+        MainMethodResult result = invokeMain("-textFile", "airtest3.txt","Salt Lake","345", "pdx", "12/12/2002",
+                "12:13","am","slc","12/12/2002", "11:20","am");
         assertThat(result.getExitCode(), equalTo(1));
         assertThat(result.getTextWrittenToStandardError(), containsString("Airline can not be updated"));
         pw.close();
@@ -89,14 +96,16 @@ class Project2IT extends InvokeMainTestCase {
     void dumperCreatesFileCommandLineArgument() throws FileNotFoundException {
         File testFile = new File("testFile");
         assertFalse(testFile.exists());
-        MainMethodResult result = invokeMain("-textFile", "testFile","Salt Lake","345", "pdx", "12/12/2002","12:13","slc","12/12/2002", "14:20");
+        MainMethodResult result = invokeMain("-textFile", "testFile","Salt Lake","345", "pdx", "12/12/2002",
+                "12:13","am","slc","12/12/2002", "11:20","am");
         assertThat(result.getExitCode(), equalTo(0));
         assertTrue(testFile.exists());
         testFile.delete();
     }
     @Test
     void dumperInvokedInMain() throws IOException {
-        MainMethodResult result = invokeMain("-textFile", "airtest4.txt", "Salt Lake", "345", "pdx", "12/12/2002", "12:13", "slc", "12/12/2002", "14:20");
+        MainMethodResult result = invokeMain("-textFile", "airtest4.txt", "Salt Lake", "345", "pdx",
+                "12/12/2002", "12:13","am", "slc", "12/12/2002", "11:20","am");
         File testFile = new File("airtest4.txt");
         BufferedReader br = new BufferedReader(new FileReader(testFile));
         assertThat(result.getExitCode(), equalTo(0));
