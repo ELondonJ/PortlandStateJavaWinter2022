0bdd494d3b68c6e42ac34afd8c98efee35191f85
London Joseph <london.joseph@pcc.edu>
2022-03-07 22:36:14 -0800

    UI for add Flight activity created. No logic/functionality

--- airline-android/app/src/main/java/edu/pdx/cs410j/ljoseph/MainActivity.java
+++ airline-android/app/src/main/java/edu/pdx/cs410j/ljoseph/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.pdx.cs410j.ljoseph;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,4 +16,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void launchFindAirline(View view) {
+        Intent intent = new Intent(MainActivity.this,FindAirline.class);
+        startActivity(intent);
+    }
+
+    public void launchFindFlights(View view) {
+        Intent intent = new Intent(MainActivity.this,FindFlights.class);
+        startActivity(intent);
+    }
+
+    public void launchAddFlights(View view) {
+        Intent intent = new Intent(MainActivity.this,AddFlights.class);
+        startActivity(intent);
+    }
 }
